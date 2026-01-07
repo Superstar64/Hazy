1485eabbc6e7fd3f3f74d57ad002c44d9fdc0612
Freddy Cubas <Hexagonalstar64@gmail.com>
2026-01-07 17:02:15 -0500

    Add hook to format Javascript
    This adds a hook that uses prettier to format any Javascript. Like
    the other formatters, the version number is pinned down.
    
    Additionally, Hazy.mjs is formatted.

--- runtime/Hazy.mjs
+++ runtime/Hazy.mjs
@@ -1,32 +1,31 @@
-export const placeholder =
-{
-    a: 0,
-    b() {
-        throw new Error("placeholder");
-    }
-}
+export const placeholder = {
+  a: 0,
+  b() {
+    throw new Error("placeholder");
+  },
+};
 
-export const numInt =
-{ a : placeholder,
-  b : placeholder,
-  c : placeholder,
-  d : placeholder,
-  e : placeholder,
-  f : placeholder,
-  g : placeholder
+export const numInt = {
+  a: placeholder,
+  b: placeholder,
+  c: placeholder,
+  d: placeholder,
+  e: placeholder,
+  f: placeholder,
+  g: placeholder,
 };
 
 export const numInteger = numInt;
 
-export const enumInt =
-{ a : placeholder,
-  b : placeholder,
-  c : placeholder,
-  d : placeholder,
-  e : placeholder,
-  f : placeholder,
-  g : placeholder,
-  h : placeholder
+export const enumInt = {
+  a: placeholder,
+  b: placeholder,
+  c: placeholder,
+  d: placeholder,
+  e: placeholder,
+  f: placeholder,
+  g: placeholder,
+  h: placeholder,
 };
 
 export const enumInteger = enumInt;
