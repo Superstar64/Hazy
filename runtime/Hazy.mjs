951656a47ee7e0c60fe80e178d3659ad53533327
Freddy Cubas <Hexagonalstar64@gmail.com>
2026-01-12 20:00:00 -0500

    Implement Num Integer
    This implements the runtime for the `Num Integer` instance. There's
    no test for this yet as the show instance isn't implemented yet.
    
    Additionally, some helpers where added to the runtime and a bug with
    placeholder being marked an evaluted was fixed.

--- runtime/Hazy.mjs
+++ runtime/Hazy.mjs
@@ -2,8 +2,23 @@ function force(thunk) {
   return thunk.a ? thunk.b() : thunk.b;
 }
 
+function strict(b) {
+  return {
+    a: 0,
+    b,
+  };
+}
+
+function strict1(f) {
+  return strict((x) => f(force(x)));
+}
+
+function strict2(f) {
+  return strict((x) => (y) => f(force(x), force(y)));
+}
+
 export const placeholder = {
-  a: 0,
+  a: 1,
   b() {
     throw new Error("placeholder");
   },
@@ -23,7 +38,15 @@ export const numInt = {
   g: placeholder,
 };
 
-export const numInteger = numInt;
+export const numInteger = {
+  a: strict2((x, y) => x + y),
+  b: strict2((x, y) => x - y),
+  c: strict2((x, y) => x * y),
+  d: strict2((x) => -x),
+  e: strict1((x) => (x >= 0n ? x : -x)),
+  f: strict1((x) => (x == 0n ? 0n : x > 0n ? 1n : -1n)),
+  g: strict1((x) => x),
+};
 
 export const enumInt = {
   a: placeholder,
@@ -39,22 +62,16 @@ export const enumInt = {
 export const enumInteger = enumInt;
 
 // todo perform replacement on invalid scalar values
-export const pack = {
-  a: 0,
-  b: (list) => {
-    let buffer = "";
-    let current = force(list);
-    while (current.a) {
-      buffer += force(current.b);
-      current = force(current.c);
-    }
-    return buffer;
-  },
-};
+export const pack = strict((list) => {
+  let buffer = "";
+  let current = force(list);
+  while (current.a) {
+    buffer += force(current.b);
+    current = force(current.c);
+  }
+  return buffer;
+});
 
-export const putStrLn = {
-  a: 0,
-  b: (thunk) => () => {
-    console.log(force(thunk));
-  },
-};
+export const putStrLn = strict((thunk) => () => {
+  console.log(force(thunk));
+});
