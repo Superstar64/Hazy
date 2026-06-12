b1b02ceb40563e3788c71fd951004f04e71280ac
Freddy Cubas <Hexagonalstar64@gmail.com>
2026-06-12 16:49:30 -0400

    Make code generation smarter about values
    This adds a peephole optimization that makes thunks not delay
    computation for values.

--- compiler/Generate/Go/Expression.hs
+++ compiler/Generate/Go/Expression.hs
@@ -3,7 +3,8 @@ module Generate.Go.Expression where
 import Control.Monad (zipWithM)
 import Control.Monad.ST (ST)
 import Core.Tree.ConstructorInfo (ConstructorInfo (..))
-import Core.Tree.EntryInfo (EntryInfo (..))
+import Core.Tree.EntryInfo (EntryInfo (EntryInfo))
+import qualified Core.Tree.EntryInfo
 import Core.Tree.Expression (Expression (..))
 import Core.Tree.Instanciation (Instanciation (Instanciation))
 import Core.Tree.MethodInfo (MethodInfo (..))
@@ -12,6 +13,7 @@ import qualified Core.Tree.SchemeOver as SchemeOver
 import Data.Char (ord)
 import Data.Foldable (toList)
 import qualified Data.Vector as Vector
+import qualified Data.Vector.Strict as Strict
 import qualified Generate.Binding.Term as Term
 import Generate.Context (Context (..), fresh, singleBinding, symbol, (!-))
 import qualified Generate.Context as Context
@@ -28,6 +30,8 @@ import qualified Javascript.Tree.Statement as Javascript (Statement (..))
 import qualified Semantic.Index.Constructor as Constructor
 import qualified Semantic.Index.Method as Method
 import qualified Semantic.Index.Selector as Selector
+import Semantic.Scope (SimpleDeclaration)
+import qualified Semantic.Scope as Scope
 
 delay :: [Javascript.Statement 'True] -> Javascript.Expression
 delay body =
@@ -59,6 +63,22 @@ delay body =
           ( Mangle.value,
             Javascript.Method
               { definition = mconcat [body, [flag], [return]]
+              }
+          )
+        ]
+   in Javascript.Object {fields}
+
+strict :: Javascript.Expression -> Javascript.Expression
+strict literal =
+  let fields =
+        [ ( Mangle.lazy,
+            Javascript.Literal
+              { literal = Javascript.Number {number = 0}
+              }
+          ),
+          ( Mangle.value,
+            Javascript.Literal
+              { literal
               }
           )
         ]
@@ -100,15 +120,94 @@ evaluate True value [] = value
 evaluate _ function arguments =
   Javascript.Call {function, arguments}
 
+generateConstructor ::
+  Context s scope ->
+  Constructor.Index scope ->
+  Strict.Vector (Expression scope) ->
+  ConstructorInfo scope ->
+  ST s ([Javascript.Statement 'True], Javascript.Expression)
+generateConstructor context Constructor.Index {constructorIndex} arguments ConstructorInfo {entries} = do
+  let process EntryInfo {strict} argument =
+        if Info.strict strict
+          then do
+            (extra, value) <- generate context argument
+            pure (extra, value)
+          else do
+            value <- thunk context Done argument
+            pure ([], value)
+  (extra, arguments) <- unzip <$> zipWithM process (toList entries) (toList arguments)
+  let tag = Javascript.Number constructorIndex
+      elements = map Javascript.Literal (tag : arguments)
+      fields = zip Mangle.fields elements
+      object = Javascript.Object {fields}
+  pure (concat extra, object)
+
+generateLambda :: Context s scope -> Expression (SimpleDeclaration 'Scope.:+ scope) -> ST s Javascript.Expression
+generateLambda context body = do
+  name <- fresh context
+  context <- pure $ singleBinding name context
+  statements <- generateInto context Return body
+  pure
+    Javascript.Arrow
+      { parameters = [name],
+        body = statements
+      }
+
+generateCharacter :: Char -> Javascript.Expression
+generateCharacter character = Javascript.Number {number = ord character}
+
+generateInteger :: Integer -> Javascript.Expression
+generateInteger integer = Javascript.BigInt {bigInt = integer}
+
+-- |
+-- Is the thunk a part of a binding group or not?
+data Binder
+  = Done
+  | Group
+
+thunk :: Context s scope -> Binder -> Expression scope -> ST s Javascript.Expression
+thunk context Done Variable {variable, instanciation = Instanciation instanciation}
+  | null instanciation,
+    Term.Binding {name, strict = False} <- context !- variable = do
+      name <- symbol context name
+      pure Javascript.Variable {name}
+thunk context binder expression = case expression of
+  Constructor {constructor, arguments, constructorInfo} -> do
+    (extra, value) <- generateConstructor context constructor arguments constructorInfo
+    if not (null extra)
+      then do
+        let body = extra ++ [Javascript.Expression Javascript.Assign {target = done, value}]
+        pure (delay body)
+      else do
+        pure (strict value)
+  Character {character} -> pure (strict $ generateCharacter character)
+  Integer {integer} -> pure (strict $ generateInteger integer)
+  Lambda {body} -> do
+    value <- generateLambda context body
+    pure $ strict value
+  Newtype {argument} -> thunk context binder argument
+  _ -> do
+    body <- generateInto context (Assign done) expression
+    pure (delay body)
+
 generate ::
   Context s scope ->
   Expression scope ->
   ST s ([Javascript.Statement 'True], Javascript.Expression)
-generate context expression = do
-  name <- fresh context
-  let letx = Javascript.Let name
-  body <- generateInto context (Assign Javascript.Variable {name}) expression
-  pure (letx : body, Javascript.Variable {name})
+generate context expression = case expression of
+  Constructor {constructor, arguments, constructorInfo} -> do
+    generateConstructor context constructor arguments constructorInfo
+  Character {character} -> pure ([], generateCharacter character)
+  Integer {integer} -> pure ([], generateInteger integer)
+  Lambda {body} -> do
+    value <- generateLambda context body
+    pure ([], value)
+  Newtype {argument} -> generate context argument
+  _ -> do
+    name <- fresh context
+    let letx = Javascript.Let name
+    body <- generateInto context (Assign Javascript.Variable {name}) expression
+    pure (letx : body, Javascript.Variable {name})
 
 generateInto ::
   Context s scope ->
@@ -122,28 +221,12 @@ generateInto context target = \case
     let expression = Javascript.Variable {name}
     arguments <- traverse (Evidence.generate context) (toList instanciation)
     pure [finish target (evaluate strict expression arguments)]
-  Constructor
-    { constructor = Constructor.Index {constructorIndex},
-      arguments,
-      constructorInfo = ConstructorInfo {entries}
-    } -> do
-      let process EntryInfo {strict} argument =
-            if Info.strict strict
-              then do
-                (extra, value) <- generate context argument
-                pure (extra, value)
-              else do
-                value <- thunk context Done argument
-                pure ([], value)
-      (extra, arguments) <- unzip <$> zipWithM process (toList entries) (toList arguments)
-      let tag = Javascript.Number constructorIndex
-          elements = map Javascript.Literal (tag : arguments)
-          fields = zip Mangle.fields elements
-          object = Javascript.Object {fields}
-      pure $ concat extra ++ [finish target object]
+  Constructor {constructor, arguments, constructorInfo} -> do
+    (extra, object) <- generateConstructor context constructor arguments constructorInfo
+    pure $ extra ++ [finish target object]
   Character {character} -> do
-    let string = Javascript.Number {number = ord character}
-    pure [finish target string]
+    let number = Javascript.Number {number = ord character}
+    pure [finish target number]
   Method
     { method = Method.Index {methodIndex},
       evidence,
@@ -209,22 +292,6 @@ generateInto context target = \case
       ]
   Hook {hook} -> Hook.generateInto context target hook
   Newtype {argument} -> generateInto context target argument
-
--- |
--- Is the thunk a part of a binding group or not?
-data Binder
-  = Done
-  | Group
-
-thunk :: Context s scope -> Binder -> Expression scope -> ST s Javascript.Expression
-thunk context Done Variable {variable, instanciation = Instanciation instanciation}
-  | null instanciation,
-    Term.Binding {name, strict = False} <- context !- variable = do
-      name <- symbol context name
-      pure Javascript.Variable {name}
-thunk context _ value = do
-  body <- generateInto context (Assign done) value
-  pure (delay body)
 
 declaration :: Context s scope -> SchemeOver Expression scope -> ST s Javascript.Expression
 declaration context scheme@SchemeOver {result = expression} = do
