ec0f55ffb2ba8a186474d66961e595448d15d4e8
Freddy Cubas <Hexagonalstar64@gmail.com>
2026-06-23 09:55:40 -0400

    Make `StringLiteral` use Text internally
    This make `StringLiteral` use `Text` unless it's characters contain
    surrogate characters.

--- compiler/Syntax/StringLiteral.hs
+++ compiler/Syntax/StringLiteral.hs
@@ -1,19 +1,30 @@
 module Syntax.StringLiteral (StringLiteral, pack, unpack) where
 
-import qualified Data.Vector.Strict as Strict (Vector)
-import qualified Data.Vector.Strict as Strict.Vector
+import qualified Data.Char as Char
+import Data.Text (Text)
+import qualified Data.Text as Text
 
 -- |
--- String literals can contain surrigate characters. So using `Text` for them
--- isn't an option.
-newtype StringLiteral = StringLiteral {runString :: Strict.Vector Char}
+-- String literals can contain surrigate characters. So using plain `Text` for
+-- them isn't an option.
+data StringLiteral
+  = String !Text
+  | Surrogate !Text !Char !StringLiteral
   deriving (Eq, Ord)
 
 instance Show StringLiteral where
   show = show . unpack
 
 pack :: String -> StringLiteral
-pack = StringLiteral . Strict.Vector.fromList
+pack string = case break surrogate string of
+  (normal, []) -> String (Text.pack normal)
+  (normal, escape : remainder) -> Surrogate (Text.pack normal) escape (pack remainder)
+  where
+    surrogate char = case Char.generalCategory char of
+      Char.Surrogate -> True
+      _ -> False
 
 unpack :: StringLiteral -> String
-unpack = Strict.Vector.toList . runString
+unpack = \case
+  String string -> Text.unpack string
+  Surrogate string char next -> Text.unpack string ++ char : unpack next
